8d01274b1c583d6d691846290a44aa0af88d9995
gustavotx <gustavo.schiffer@gmail.com>
2015-10-18 15:09:09 -0200

    Novos treinos

--- batch_test.sh
+++ batch_test.sh
@@ -3,9 +3,9 @@
 make release;
 
 # teste sem cortar blobs e sem dist transf
-./segmenter 8 treinos/caju_20im.xml 0 10 0 0
+./segmenter 6 treinos/pessego_10im.xml 0 10 0 0
 # teste sem cortar blobs e com dist transf
-./segmenter 8 treinos/caju_20im.xml 0 10 0 1
+./segmenter 6 treinos/pessego_10im.xml 0 10 0 1
 
 
 for i in 0 1 # dist transf ou nao
@@ -14,7 +14,7 @@ do
 	do
 		for k in 10 15 20 25 30 35 40 # threshold de corte
 		do
-			./segmenter 8 treinos/caju_20im.xml $j $k 1 $i
+			./segmenter 6 treinos/pessego_10im.xml $j $k 1 $i
 		done
 	done
 done
